89744d5d05a85b335ab9b21c22865955de45d2b3
LuanComputacao <eng.luanroger@gmail.com>
2023-01-17 01:04:04 -0300

    feat: comment the app quit when idle

--- src/main/browser_window_blur.js
+++ src/main/browser_window_blur.js
@@ -3,7 +3,6 @@ const idleTime = require('../config').idleTime
 function quitApp (app) {
   setTimeout(() => {
     console.log(`Quitting app by idleing for ${idleTime}ms`)
-    console.log(app)
     // App.quit()
   }, idleTime)
 }
