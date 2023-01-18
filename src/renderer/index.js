0bb689994b31a4ef2e941c45e6d4f115c2ea8d37
LuanComputacao <eng.luanroger@gmail.com>
2023-01-18 11:40:11 -0300

    feat: add description to renderer script

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,1 +1,4 @@
-console.log('Hello from renderer.js')
+/** This file is required by the index.html file and will
+ * be executed in the renderer process for that window.
+ * All the Node.js APIs are available in this process.
+ */
