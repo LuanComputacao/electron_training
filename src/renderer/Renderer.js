a41283261b978f5889a80eabf5944e3c061ba25d
LuanComputacao <eng.luanroger@gmail.com>
2023-01-19 12:41:25 -0300

    refactor: encapsulates the main routine into the Renderer node module

---
+++ src/renderer/Renderer.js
@@ -0,0 +1,25 @@
+/** This file is required by the index.html file and will
+ * be executed in the renderer process for that window.
+ * All the Node.js APIs are available in this process.
+ */
+
+const { webFrame } = require('electron')
+const Ipc = require('./ipc/index')
+
+module.exports = {
+  main: function () {
+    console.log('webFrame', webFrame)
+    console.log('zoom factor', webFrame.getZoomFactor())
+    console.log('zoom level', webFrame.getZoomLevel())
+
+    console.log(process)
+    console.log(process.getuid())
+    Ipc.desktopCapturer()
+    Ipc.screenShot(
+      { types: ['screen'] }
+    )
+
+    Ipc.talk()
+    Ipc.talkResponse()
+  }
+}
